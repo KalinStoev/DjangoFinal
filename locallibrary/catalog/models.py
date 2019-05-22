a9ed72973f02a0b7d7f756e571b4cb4c35eea43e
Kalin Stoev <kalin.stoev@ffwagency.com>
2019-05-22 15:20:21 +0300

    Initial Project: Models, View, Templates for Book Catalog

---
+++ locallibrary/catalog/models.py
@@ -0,0 +1,111 @@
+from django.db import models
+
+# Create your models here.
+
+from django.urls import reverse  # To generate URLS by reversing URL patterns
+
+
+import uuid  # Required for unique book instances
+from datetime import date
+
+from django.contrib.auth.models import User
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=200,
+        help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
+        )
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, null=True)
+    # Foreign Key used because book can only have one author, but authors can have multiple books
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    isbn = models.CharField('ISBN', max_length=13,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                                      '">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    # ManyToManyField used because a genre can contain many books and a Book can cover many genres.
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, null=True)
+
+    def display_genre(self):
+        """Creates a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
+    display_genre.short_description = 'Genre'
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular book instance. Because I don't like regex :)"""
+        return reverse('book-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.title
+
+
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
+    LOAN_STATUS = (
+        ('d', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='a',
+        help_text='Book availability')
+
+    # Order records when they are returned in a query and
+    class Meta:
+        ordering = ['due_back']
+        # permissions = (("can_mark_returned", "Set book as returned"),)
+
+    def __str__(self):
+        return f'{self.id}. {self.book.title}'
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('died', null=True, blank=True)
+
+    # Order records when they are returned in a query
+    class Meta:
+        ordering = ['first_name', 'last_name']
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance. Because I don't like regex :)"""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
